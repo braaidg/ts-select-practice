36b8baddc991641263a10289baf92e0059797675
braaidg <braiandenisgomez@gmail.com>
2023-03-26 01:30:36 -0300

    First part

---
+++ src/Select.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react';
+import styles from './Select.module.css';
+
+type SelectOption = {
+  label: string
+  value: string | number
+}
+
+type SelectProps = {
+  options: SelectOption[]
+  value?: SelectOption
+  onChange: (value: SelectOption | undefined) => void
+}
+
+export function Select({ value, onChange, options }: SelectProps) {
+
+  const [isOpen, setIsOpen] = useState(false)
+  const [highlightedIndex, setHighlightedIndex] = useState(0)
+  function clearOptions() {
+    onChange(undefined)
+  }
+
+  function selectOption(option: SelectOption) {
+    if (option !== value) onChange(option)
+  }
+
+  function isOptionSelected(option: SelectOption) {
+    return option === value;
+  }
+
+  useEffect(() => {
+    if (isOpen) {
+      setHighlightedIndex(0)
+    }
+  }, [isOpen])
+
+  return (
+    <div tabIndex={0} className={styles.container} onClick={() => setIsOpen(prev => !prev)} onBlur={() => setIsOpen(false)}>
+      <span className={styles.value}>{value?.label}</span>
+      <button className={styles["clear-btn"]} onClick={e => {
+        e.stopPropagation()
+        clearOptions()
+      }} >&times;</button>
+      <div className={styles.caret}></div>
+      <ul className={`${styles.options} ${isOpen ? styles.show : ""}`}>
+        {options.map((option, index) => (
+          <li key={option.label} className={`${styles.option} ${isOptionSelected(option) ? styles.selected : ""} ${index === highlightedIndex ? styles.highlighted : ""}` } onClick={e => {
+            e.stopPropagation()
+            selectOption(option)
+            setIsOpen(false)
+          }}
+          onMouseEnter={()=> setHighlightedIndex(index)}
+          >
+            {option.label}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+ }
